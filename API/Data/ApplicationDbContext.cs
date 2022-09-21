03f4299e30f722084595ccd3186eefad7af32da1
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-09-21 16:38:35 -0500

    Added account controller

--- API/Data/ApplicationDbContext.cs
+++ API/Data/ApplicationDbContext.cs
@@ -18,29 +18,29 @@ namespace API.Data
         public DbSet<AppUser> Users { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<AppUser>().HasData(
-                new AppUser
-                {
-                    Id = 1,
-                    UserName = "Tom",
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     base.OnModelCreating(modelBuilder);
+        //     modelBuilder.Entity<AppUser>().HasData(
+        //         new AppUser
+        //         {
+        //             Id = 1,
+        //             UserName = "Tom",
                    
-                },
-                new AppUser
-                {
-                    Id = 2,
-                    UserName = "Bob",
+        //         },
+        //         new AppUser
+        //         {
+        //             Id = 2,
+        //             UserName = "Bob",
                    
-                },
-                new AppUser
-                {
-                    Id = 3,
-                    UserName = "Ruth",
+        //         },
+        //         new AppUser
+        //         {
+        //             Id = 3,
+        //             UserName = "Ruth",
                    
-                });
-        }
+        //         });
+        // }
         
     }
 }
